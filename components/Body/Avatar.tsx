72124db35c219fc24e7a20c9d5aae7d2d57678bb
Dmitry Ordin <60065455+qstyle@users.noreply.github.com>
2022-09-25 14:43:44 +0300

    feat(prod): aad skills (#6)

--- components/Body/Avatar.tsx
+++ components/Body/Avatar.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import fs from 'fs'
 import Image from 'next/image'
-import AvatarSrc from '../../public/avatar/4D5q2Z3otpE.jpg'
+import { Image as ChakraImage } from '@chakra-ui/react'
+import AvatarSrc from '../../public/avatar/avatar.jpg'
 // import {Image} from '@chakra-ui/react'
 
-export const Avatar: React.FC = ( ) => {
-// var files = fs.readdirSync()
-
-// console.log(files)
-
-
-  return(
-    <Image
-
-      width='500px'
-      height='500px'
-      src={AvatarSrc}
-      alt="avatar" />
-  )
-}
+export const Avatar: React.FC = () => (
+  <Image
+    width='400px'
+    height='500px'
+    src={AvatarSrc}
+    alt="avatar" />
+)
---
+++ components/Body/Skills/data.ts
@@ -0,0 +1,35 @@
+export const MainHardSkills = [
+  'Html',
+  'GIT',
+  'CSS, SASS, LESS',
+  'Java script',
+  'Node JS',
+  'React',
+  'GraphQL',
+  'Effector',
+  'Apollo',
+  'Next JS (SSR)',
+  'UI design system'
+]
+
+export const ProgressHardSkills = [
+  'Redux, Mobx',
+  'Node JS',
+  'Nest JS',
+  'PostgreSQL',
+]
+
+export const SoftSkills = [
+  'Accurate',
+  'Calm',
+  'Confident',
+  'Reliable',
+  'Articulate',
+  'Eager to learn',
+  'Diligent',
+]
+
+export const Language = [
+  'Russian',
+  'English (read and write)'
+]
